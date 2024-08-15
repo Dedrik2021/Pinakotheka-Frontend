aa2f9dd076be58af4386fae50aa6f51cf7e32168
Oleg Grunski <oleggr578@gmail.com>
2024-08-15 22:39:21 +0200

    created painting column card component

--- src/components/card/paintingColumnCard/PaintingColumnCard.jsx
+++ src/components/card/paintingColumnCard/PaintingColumnCard.jsx
@@ -1,101 +1,15 @@
-// import { Link } from 'react-router-dom';
-
-// import SkeletonPaintingCard from '../../skeletons/SkeletonPaintingCard';
-
-// import './paintingColumnCard.scss';
-
-// const PaintingColumnCard = (props) => {
-// 	const {
-// 		_id,
-// 		authorName,
-// 		createdAt,
-// 		description,
-// 		loading,
-// 		status,
-// 		image,
-// 		material,
-// 		price,
-// 		size,
-// 		name,
-// 	} = props;
-
-// 	const options = {
-// 		year: 'numeric',
-// 		month: 'long',
-// 		day: 'numeric',
-// 		hour: '2-digit',
-// 		minute: '2-digit',
-// 		second: '2-digit',
-// 		timeZone: 'UTC',
-// 		timeZoneName: 'short',
-// 	};
-
-// 	const date = new Date(createdAt);
-
-// 	return (
-// 		<li className="painting-column-card__item">
-// 			{status === 'loading' || loading ? (
-// 				<div style={{ backgroundColor: '#fff', borderRadius: '5px' }}>
-// 					<SkeletonPaintingCard />
-// 				</div>
-// 			) : (
-// 				<article className="card">
-// 					<Link to={''} className="card__link card__link--painting">
-// 						<div className="card__img-wrapper">
-// 							<img height={500} width={460} src={image} alt={name} />
-// 						</div>
-// 					</Link>
-// 					<div className="card__content">
-// 						<span className="">
-//                         <span className='card__item'>Lot: </span> {_id}</span>
-// 						<Link className="card__title-link" to={''}>
-// 							<h2 className="card__title">{name}</h2>
-// 						</Link>
-// 						<div className="card__wrapper">
-// 							<div className='card__author-wrapper'>
-// 								<span className="card__item">Author: </span>
-// 								<Link className="card__author" to={''}>
-// 									<span className="card__author-name"> {authorName}</span>
-// 								</Link>
-// 							</div>
-// 							<div>Rating</div>
-// 						</div>
-// 						<div className="card__wrapper">
-// 							<div className="card__items">
-// 								<div className="card__material">
-// 									<span className="card__item">Material: </span>
-// 									{material},
-// 								</div>
-// 								<div>
-// 									<span className="card__item">Size:</span> ({size})
-// 								</div>
-// 							</div>
-// 							<div>
-// 								<span className="card__item">Price:</span> €{price}
-// 							</div>
-// 						</div>
-// 						<div className='card__descr-wrapper'>
-// 							<span className="card__item">Description: </span>
-// 							<p> {description}</p>
-// 						</div>
-// 						<div>
-// 							<span>
-// 								<span className="card__item">Added: </span>
-// 								{date.toLocaleDateString(options)}
-// 							</span>
-// 						</div>
-// 					</div>
-// 				</article>
-// 			)}
-// 		</li>
-// 	);
-// };
-
-// export default PaintingColumnCard;
-
 import { Link } from 'react-router-dom';
 
 import SkeletonHorizontalCard from '../../skeletons/SkeletonHorizontalCard';
+import { isNew } from '../../../utils/helper';
+import {
+	paintingSize,
+	sculptureSize,
+	drawingSize,
+	digitalArtSize,
+	handmadeSize,
+} from '../../../utils/sizes';
+import { capitalizeFirstLetter } from '../../../utils/helper';
 
 import './paintingColumnCard.scss';
 
@@ -103,6 +17,8 @@ const PaintingColumnCard = (props) => {
 	const {
 		_id,
 		authorName,
+		authorId,
+		path,
 		createdAt,
 		description,
 		loading,
@@ -110,8 +26,11 @@ const PaintingColumnCard = (props) => {
 		image,
 		material,
 		price,
+		style,
 		size,
+		sale,
 		name,
+		slug = '',
 	} = props;
 
 	const options = {
@@ -128,62 +47,99 @@ const PaintingColumnCard = (props) => {
 	const date = new Date(createdAt);
 
 	return (
-                <>
-		<li className="painting-column-card__item">
-			{status === 'loading' || loading ? (
-				<div style={{ backgroundColor: '#fff', borderRadius: '5px' }}>
-					<SkeletonHorizontalCard />
-				</div>
-			) : (
-				<article className="card">
-					<Link to={''} className="card__link card__link--painting">
-						<div className="card__img-wrapper">
-							<img height={500} width={460} src={image} alt={name} />
-						</div>
-					</Link>
-					<div className="card__content">
-						<span className="card__lot">
-							<span className="card__item">Lot: </span> {_id}
-						</span>
-						<Link className="card__title-link" to={''}>
-							<h2 className="card__title">{name}</h2>
+		<>
+			<li className="painting-column-card__item">
+				{status === 'loading' || loading ? (
+					<div style={{ backgroundColor: '#e7e7e7', borderRadius: '5px', width: '100%' }}>
+						<SkeletonHorizontalCard />
+					</div>
+				) : (
+					<article className="card">
+						<Link to={`${path}${_id}`} className="card__link card__link--painting">
+							<div className="card__img-wrapper">
+								{isNew(date) ? <div className="card__label-new">New</div> : null}
+								<img height={380} width={550} src={image} alt={name} />
+								<div
+									className="card__img-copy"
+									style={{ backgroundImage: `url(${image})` }}
+								></div>
+							</div>
 						</Link>
+						<div className="card__content">
+							<span className="card__lot">
+								<span className="card__item">Lot: </span> {_id}
+							</span>
+							<Link className="card__title-link" to={`${path}${_id}`}>
+								<h2 className="card__title title">{name}</h2>
+							</Link>
 							<div className="card__author-wrapper">
 								<span className="card__item">Author: </span>
-								<Link className="card__author" to={''}>
+								<Link className="card__author" to={`/single-user/${authorId}`}>
 									<span className="card__author-name"> {authorName}</span>
 								</Link>
 							</div>
-							<div className='card__rating'>Rating</div>
-							<div className="card__items">
-								<div className="card__material">
-									<span className="card__item">Material: </span>
-									{material},
-								</div>
-								<div className='card__size'>
-									<span className="card__item">Size:</span> ({size})
-								</div>
+							<div className="card__rating">Rating</div>
+							<div className="card__material">
+								<span className="card__item">Material: </span>
+								<ul className="card__list">
+									{material?.map((item, i) => {
+										return (
+											<li key={i} className="card__element-item">
+												<span>[{capitalizeFirstLetter(item)}]</span>
+											</li>
+										);
+									})}
+								</ul>
 							</div>
-							<div className='card__price'>
-								<span className="card__item">Price:</span> €{price}
+							<div className="card__style">
+								<span className="card__item">Style:</span>
+								<ul className="card__list">
+									{style?.map((item, i) => {
+										return (
+											<li key={i} className="card__element-item">
+												[{capitalizeFirstLetter(item)}]
+											</li>
+										);
+									})}
+								</ul>
 							</div>
-						<div className="card__descr-wrapper">
-							<span className="card__item">Description: </span>
-							<p> {description}</p>
+							<div className="card__size">
+								<span className="card__item">Size:</span>
+								{slug === 'paintings' && paintingSize(size)}
+								{slug === 'sculptures' && sculptureSize(size)}
+								{slug === 'drawings' && drawingSize(size)}
+								{slug === 'digital-arts' && digitalArtSize(size)}
+								{slug === 'handmades' && handmadeSize(size)}
+							</div>
+							<div className="card__price">
+								<div className="card__price-wrapper">
+									<span className="card__item">Price:</span>
+									<div className={` ${sale ? 'card__price--strike' : ''}`}>
+										€{price}
+									</div>
+								</div>
+								{sale && (
+									<div className="card__sale">
+										<span className="card__item">Sale:</span>€{sale}
+									</div>
+								)}
+							</div>
+							<div className="card__descr-wrapper">
+								<span className="card__item">Description: </span>
+								<p> {description}</p>
+							</div>
+							<div>
+								<span>
+									<span className="card__item">Date: </span>
+									{date.toLocaleDateString(options)}
+								</span>
+							</div>
 						</div>
-						<div>
-							<span>
-								<span className="card__item">Added: </span>
-								{date.toLocaleDateString(options)}
-							</span>
-						</div>
-					</div>
-				</article>
-			)}
-		</li>
-                {/* <SkeletonHorizontalCard /> */}
-
-                </>
+					</article>
+				)}
+			</li>
+			{/* <SkeletonHorizontalCard /> */}
+		</>
 	);
 };
 
