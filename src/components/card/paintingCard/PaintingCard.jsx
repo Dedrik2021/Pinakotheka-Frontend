83ed17c66bc4dc032f0c10bdfc0068d348691c2b
Oleg Grunski <oleggr578@gmail.com>
2024-08-22 23:03:16 +0200

    added dependencies

--- src/components/card/paintingCard/PaintingCard.jsx
+++ src/components/card/paintingCard/PaintingCard.jsx
@@ -2,14 +2,6 @@ import { Link } from 'react-router-dom';
 
 import SkeletonPaintingCard from '../../skeletons/SkeletonPaintingCard';
 import { isNew } from '../../../utils/helper';
-import {
-	paintingSize,
-	sculptureSize,
-	drawingSize,
-	digitalArtSize,
-	handmadeSize,
-} from '../../../utils/sizes';
-import { capitalizeFirstLetter } from '../../../utils/helper';
 
 import './paintingCard.scss';
 
@@ -21,14 +13,13 @@ const PaintingCard = (props) => {
 		loading,
 		status,
 		image,
-		material,
 		price,
-		size,
 		sale,
 		name,
 		path,
+		page = '',
+		singleArtId = null,
 		createdAt,
-		slug = '',
 		background = '#fff',
 	} = props;
 
@@ -36,19 +27,25 @@ const PaintingCard = (props) => {
 
 	return (
 		<>
-			<li className="painting-card__item">
+			<li className={`painting-card__item ${singleArtId === _id ? 'active' : ''}`}>
 				{status === 'loading' || loading ? (
 					<SkeletonPaintingCard />
 				) : (
-					<article className="card" style={{ backgroundColor: `${background}` }}>
-						<Link to={`${path}${_id}`} className="card__link card__link--painting">
+					<article className={`card`} style={{ backgroundColor: `${background}` }}>
+						<Link
+							to={`${path}${_id}${page !== '' ? `?page=${page}` : ''}`}
+							className="card__link card__link--painting"
+						>
 							<div className="card__img-wrapper">
 								{isNew(date) ? <div className="card__label-new">New</div> : null}
 								<img height={320} width={260} src={image} alt={name} />
 							</div>
 						</Link>
 						<div className="card__content">
-							<Link className="card__title-link" to={`${path}${_id}`}>
+							<Link
+								className="card__title-link"
+								to={`${path}${_id}${page !== '' ? `?page=${page}` : ''}`}
+							>
 								<h2 className="card__title title">{name}</h2>
 							</Link>
 							<div className="card__items">
