a56a88be422d43f8474a75620a0293cd8adc2121
Oleg Grunski <oleggr578@gmail.com>
2024-10-23 21:35:56 +0200

    added sceleton painting card

--- src/components/skeletons/SkeletonPaintingCard.jsx
+++ src/components/skeletons/SkeletonPaintingCard.jsx
@@ -1,22 +1,22 @@
 import React from 'react';
 import ContentLoader from 'react-content-loader';
 
-const SkeletonPaintingCard = (props) => (
+const SkeletonPaintingCard = ({height = 413}) => (
 	<ContentLoader
 		speed={2}
 		width={262}
-		height={393}
-		viewBox="0 0 262 393"
+		height={height}
+		viewBox="0 0 262 413"
 		backgroundColor="#b3b2b2"
 		foregroundColor="#f2eeee"
-		{...props}
+		
 	>
-		<rect x="5" y="5" rx="5" ry="5" width="255" height="300" />
-		<rect x="10" y="320" rx="5" ry="5" width="130" height="15" />
-		<rect x="10" y="345" rx="5" ry="5" width="130" height="15" />
-		<rect x="160" y="345" rx="5" ry="5" width="100" height="15" />
-		<rect x="10" y="370" rx="5" ry="5" width="130" height="15" />
-		<rect x="160" y="370" rx="5" ry="5" width="100" height="15" />
+		<rect x="5" y="5" rx="5" ry="5" width="253" height="319" />
+		<rect x="10" y="337" rx="5" ry="5" width="180" height="20" />
+		<rect x="10" y="370" rx="5" ry="5" width="130" height="10" />
+		<rect x="160" y="370" rx="5" ry="5" width="100" height="10" />
+		<rect x="10" y="395" rx="5" ry="5" width="130" height="10" />
+		<rect x="180" y="395" rx="5" ry="5" width="80" height="10" />
 	</ContentLoader>
 );
 
