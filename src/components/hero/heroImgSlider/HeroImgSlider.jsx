c43ec0362d4145e71818f1bf6c562262657b3ced
Oleg Grunski <oleggr578@gmail.com>
2024-12-05 23:19:56 +0100

    fixed bug

--- src/components/hero/heroImgSlider/HeroImgSlider.jsx
+++ src/components/hero/heroImgSlider/HeroImgSlider.jsx
@@ -3,7 +3,7 @@ import { useState, useEffect } from 'react';
 
 import { getImageDimensions } from '../../../utils/helper';
 import img_frame_horizontal from '../../../assets/images/frame_horizontal.png';
-import img_frame_vertical from '../../../assets/images/frame_vertical.png'
+import img_frame_vertical from '../../../assets/images/frame_vertical.png';
 
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
@@ -13,6 +13,7 @@ import './heroImgSlider.scss';
 const HeroImgSlider = (props) => {
 	const { imgSlider, sliderInfo, setSliderImg, paintings } = props;
 	const [imageDimensions, setImageDimensions] = useState({});
+	const [imgCheck, setImgCheck] = useState(false);
 
 	useEffect(() => {
 		const fetchImageDimensions = async () => {
@@ -31,6 +32,14 @@ const HeroImgSlider = (props) => {
 		fetchImageDimensions();
 	}, []);
 
+	useEffect(() => {
+		if (Object.keys(imageDimensions).length > 0) {
+			setTimeout(() => {
+				setImgCheck(true);
+			}, 0);
+		}
+	}, [imageDimensions]);
+
 	return (
 		<div className="container container--lg">
 			<Slider
@@ -43,58 +52,80 @@ const HeroImgSlider = (props) => {
 				{paintings?.map(({ _id, image, title }) => {
 					return (
 						<li
-							className={`hero__item hero__item--img ${imageDimensions[_id]?.width > imageDimensions[_id]?.height
-											? "big" : ""}`}
+							className={`hero__item hero__item--img ${
+								imageDimensions[_id]?.width > imageDimensions[_id]?.height
+									? 'big'
+									: 'small'
+							}`}
 							key={_id}
 						>
-							<div
-								className="hero__img-wrapper"
-								style={{
-									width:
-										imageDimensions[_id]?.width < imageDimensions[_id]?.height
-											? 400
-											: 900,
-								}}
-							>
+							{imgCheck ? (
 								<div
-									className="hero__frame"
+									className="hero__img-wrapper"
 									style={{
-										backgroundImage: `url(${imageDimensions[_id]?.width > imageDimensions[_id]?.height ? img_frame_horizontal : img_frame_vertical })`,
 										width:
 											imageDimensions[_id]?.width <
 											imageDimensions[_id]?.height
-												? 620
-												: 1120,
-										height: imageDimensions[_id]?.width <
-											imageDimensions[_id]?.height
-												? 800
-												: 760,
-										left: imageDimensions[_id]?.width <
-											imageDimensions[_id]?.height
-												? -110
-												: -175,
-												top: imageDimensions[_id]?.width <
-											imageDimensions[_id]?.height
-												? -125
-												: -135,
+												? 400
+												: 900,
 									}}
-								></div>
-								<img
-									src={image}
-									alt={title}
-									height={imageDimensions[_id]?.width > imageDimensions[_id]?.height
-											? 500
-											: 550}
-									width={
-										imageDimensions[_id]?.width < imageDimensions[_id]?.height
-											? 400
-											: 770
-									}
-									style={{boxShadow: imageDimensions[_id]?.width < imageDimensions[_id]?.height
-											? '38px 20px 10px 47px rgba(0, 0, 0, 0.5)'
-											: '38px 32px 10px 55px rgba(0, 0, 0, 0.5)'}}
-								/>
-							</div>
+								>
+									<div
+										className="hero__frame"
+										style={{
+											backgroundImage: `url(${
+												imageDimensions[_id]?.width >
+												imageDimensions[_id]?.height
+													? img_frame_horizontal
+													: img_frame_vertical
+											})`,
+											width:
+												imageDimensions[_id]?.width <
+												imageDimensions[_id]?.height
+													? 620
+													: 1120,
+											height:
+												imageDimensions[_id]?.width <
+												imageDimensions[_id]?.height
+													? 880
+													: 760,
+											left:
+												imageDimensions[_id]?.width <
+												imageDimensions[_id]?.height
+													? -110
+													: -175,
+											top:
+												imageDimensions[_id]?.width <
+												imageDimensions[_id]?.height
+													? -145
+													: -135,
+										}}
+									></div>
+									<img
+										src={image}
+										alt={title}
+										height={
+											imageDimensions[_id]?.width >
+											imageDimensions[_id]?.height
+												? 500
+												: 600
+										}
+										width={
+											imageDimensions[_id]?.width <
+											imageDimensions[_id]?.height
+												? 400
+												: 770
+										}
+										style={{
+											boxShadow:
+												imageDimensions[_id]?.width <
+												imageDimensions[_id]?.height
+													? '35px 21px 10px 47px rgba(0, 0, 0, 0.5)'
+													: '38px 32px 10px 55px rgba(0, 0, 0, 0.5)',
+										}}
+									/>
+								</div>
+							) : null}
 						</li>
 					);
 				})}
