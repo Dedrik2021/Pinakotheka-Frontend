fa6fdc9085411b345110e663b45df5acd4be8b6a
Oleg Grunski <oleggr578@gmail.com>
2024-12-13 23:49:20 +0100

    added functionality

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,26 +1,39 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
+import { Helmet } from 'react-helmet';
+import PulseLoader from 'react-spinners/PulseLoader';
 
 import Spinner from '../../components/spinner/Spinner';
 import SocketContext from '../../context/SocketContext';
 import Hero from '../../components/hero/Hero';
 import PaintingsCards from '../../components/card/paintingsCards/PaintingsCards';
+import Categories from '../../components/categories/Categories';
 
 const Home = ({ socket }) => {
-	const { paintings, status, filteredPaintings, statusFiltering} = useSelector((state) => state.painting);
+	const { paintings, status, filteredPaintings, statusFiltering } = useSelector(
+		(state) => state.painting,
+	);
 	const { user } = useSelector((state) => state.user);
 
-	// useEffect(() => [window.scrollTo(0, 0)], []);
+	useEffect(() => window.scrollTo(0, 0), []);
 
-	if (status === 'loading') {
+	if (!status || status === 'loading') {
 		return <Spinner />;
 	} 
+	
 
 	return (
-		<div>
-			<Hero paintings={paintings}  />
-			<PaintingsCards paintings={paintings} />
-		</div>
+		<>
+			<Helmet>
+				<meta name="Pinakotheka | Home" />
+				<title>Pinakotheka | Home</title>
+			</Helmet>
+			<div>
+				<Hero paintings={paintings} />
+				<PaintingsCards />
+				<Categories/>
+			</div>
+		</>
 	);
 };
 
