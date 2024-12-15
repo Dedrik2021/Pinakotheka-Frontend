7059986f67ab391ce5356b8f73ed5e474bba2529
Oleg Grunski <oleggr578@gmail.com>
2024-12-16 00:20:31 +0100

    added functionality in the file userSlice

--- src/redux/slices/userSlice.js
+++ src/redux/slices/userSlice.js
@@ -1,24 +1,44 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-const apiUrl = `http://localhost:8080/api/v1/auth`;
-// const apiUrl = `${process.env.REACT_APP_API_URL}/auth`;
+// const apiUrl = `http://localhost:8080/api/v1/auth`;
+const apiUrl = `${process.env.REACT_APP_API_URL}/auth`;
 
 const initialState = {
 	status: '',
 	error: '',
+	users: [],
+	author: {},
+	onlineUsers: [],
 	user: {
 		id: '',
 		name: '',
 		email: '',
 		picture: '',
 		token: '',
-        author: false,
-        customer: false,
-        politics: false,
+		about: '',
+		author: false,
+		customer: false,
+		facebook: '',
+		twitter: '',
+		instagram: '',
+		politics: false,
+		unreadMessages: {},
 		isEmailVerified: JSON.parse(localStorage.getItem('isEmailVerified')) || false,
 	},
 };
+
+export const refreshUser = createAsyncThunk(
+	'user/refreshuser',
+	async (userId, { rejectWithValue }) => {
+		try {
+			const { data } = await axios.get(`${apiUrl}/refresh-user-by-id/${userId}`);
+			return data;
+		} catch (error) {
+			return rejectWithValue(error.response.data.error.message);
+		}
+	},
+); 
 
 //derzit polzovatelja gde libo bez useefecta
 export const registerUser = createAsyncThunk(
@@ -28,7 +48,7 @@ export const registerUser = createAsyncThunk(
 			const { data } = await axios.post(`${apiUrl}/register`, values);
 			return data;
 		} catch (error) {
-			return rejectWithValue(error.response.data.error.message)
+			return rejectWithValue(error.response.data.error.message);
 		}
 	},
 );
@@ -42,27 +62,71 @@ export const loginUser = createAsyncThunk('auth/login', async (values, { rejectW
 	}
 });
 
-export const forgotPassword = createAsyncThunk('auth/forgotpassword', async (email, { rejectWithValue }) => {
-	try {
-		const { data } = await axios.post(`${apiUrl}/forgot-password`, email);
-		return data;
-	} catch (error) {
-		return rejectWithValue(error.response.data.error.message);
-	}
-});
+export const forgotPassword = createAsyncThunk(
+	'auth/forgotpassword',
+	async (email, { rejectWithValue }) => {
+		try {
+			const { data } = await axios.post(`${apiUrl}/forgot-password`, email);
+			return data;
+		} catch (error) {
+			return rejectWithValue(error.response.data.error.message);
+		}
+	},
+);
 
-export const resetPassword = createAsyncThunk('auth/resetpassword', async (values, { rejectWithValue }) => {
-	try {
-		const { data } = await axios.post(`${apiUrl}/reset-password`, values);
-		return data;
-	} catch (error) {
-		return rejectWithValue(error.response.data.error.message);
-	}
-});
+export const getAuthorById = createAsyncThunk(
+	'user/getbyidauthor',
+	async (authorId, { rejectWithValue }) => {
+		try {
+			const { data } = await axios.get(`${apiUrl}/get-author-by-id/${authorId}`);
+			return data;
+		} catch (error) {
+			return rejectWithValue(error.response.data.error.message);
+		}
+	},
+);
 
-export const verifyEmail = createAsyncThunk('auth/verifyemail', async (values, { rejectWithValue }) => {
+export const resetPassword = createAsyncThunk(
+	'auth/resetpassword',
+	async (values, { rejectWithValue }) => {
+		try {
+			const { data } = await axios.post(`${apiUrl}/reset-password`, values);
+			return data;
+		} catch (error) {
+			return rejectWithValue(error.response.data.error.message);
+		}
+	},
+);
+
+export const updateUnreadMessage = createAsyncThunk(
+	'auth/updateunreadmessage',
+	async (values, { rejectWithValue }) => {
+		try {
+			const { data } = await axios.put(`${apiUrl}/update-unread-messages/${values.userId}/${values.senderId}`);
+			return data;
+		} catch (error) {
+			return rejectWithValue(error.response.data.error.message);
+		}
+	},
+);
+
+export const verifyEmail = createAsyncThunk(
+	'auth/verifyemail',
+	async (values, { rejectWithValue }) => {
+		try {
+			const { data } = await axios.get(`${apiUrl}/verify-email`, {
+				params: { token: values.token, id: values.id },
+			});
+			return data;
+		} catch (error) {
+			return rejectWithValue(error.response.data.error.message);
+		}
+	},
+);
+
+export const getUsers = createAsyncThunk('user/getusers', async (_, { rejectWithValue }) => {
 	try {
-		const { data } = await axios.get(`${apiUrl}/verify-email`, {params: {token: values.token, id: values.id}});
+		const { data } = await axios.get(`${apiUrl}/get-users`);
 		return data;
 	} catch (error) {
 		return rejectWithValue(error.response.data.error.message);
@@ -86,20 +150,29 @@ export const userSlice = createSlice({
 				name: '',
 				email: '',
 				picture: '',
-                author: false,
-                customer: false,
-                politics: false,
+				author: false,
+				customer: false,
+				about: '',
+				politics: false,
+				facebook: '',
+				twitter: '',
+				instagram: '',
 				token: '',
+				unreadMessages: {},
 				isEmailVerified: JSON.parse(localStorage.getItem('isEmailVerified')) || false,
 			};
 		},
-        setError: (state, action) => {
-            state.error = action.payload
-        },
+		setError: (state, action) => {
+			state.error = action.payload;
+		},
 
-        setStatus: (state, action) => {
-            state.status = action.payload
-        }
+		setStatus: (state, action) => {
+			state.status = action.payload;
+		},
+
+		setOnlineUsers: (state, action) => {
+			state.onlineUsers = action.payload
+		}
 	},
 	extraReducers: (builder) => {
 		builder
@@ -111,14 +184,13 @@ export const userSlice = createSlice({
 				state.status = 'succeeded';
 				state.error = '';
 				state.user = action.payload.user;
-				state.user.isEmailVerified = false
+				state.user.isEmailVerified = false;
 				localStorage.setItem('isEmailVerified', false);
 			})
 			.addCase(registerUser.rejected, (state, action) => {
 				state.status = 'failed';
 				state.error = action.payload;
 			})
-
 
 			.addCase(loginUser.pending, (state) => {
 				state.status = 'loading';
@@ -131,6 +203,21 @@ export const userSlice = createSlice({
 			})
 			.addCase(loginUser.rejected, (state, action) => {
 				state.status = 'failed';
+				state.error = action.payload;
+			})
+
+
+			.addCase(refreshUser.pending, (state) => {
+				// state.status = 'loading';
+				state.error = '';
+			})
+			.addCase(refreshUser.fulfilled, (state, action) => {
+				// state.status = 'succeeded';
+				state.error = '';
+				state.user = action.payload.user;
+			})
+			.addCase(refreshUser.rejected, (state, action) => {
+				// state.status = 'failed';
 				state.error = action.payload;
 			})
 
@@ -150,7 +237,6 @@ export const userSlice = createSlice({
 				state.user = action.payload.user;
 			})
 
-
 			.addCase(resetPassword.pending, (state) => {
 				state.status = 'loading';
 				state.error = '';
@@ -165,7 +251,6 @@ export const userSlice = createSlice({
 				state.error = action.payload;
 			})
 
-
 			.addCase(verifyEmail.pending, (state) => {
 				state.status = 'loading';
 				state.error = '';
@@ -173,15 +258,57 @@ export const userSlice = createSlice({
 			.addCase(verifyEmail.fulfilled, (state, action) => {
 				state.status = 'succeeded';
 				state.error = '';
-				state.user.isEmailVerified = true
+				state.user.isEmailVerified = true;
 				localStorage.setItem('isEmailVerified', true);
 			})
 			.addCase(verifyEmail.rejected, (state, action) => {
+				state.status = 'failed';
+				state.error = action.payload;
+			})
+
+			.addCase(getUsers.pending, (state) => {
+				state.status = 'loading';
+				state.error = '';
+			})
+			.addCase(getUsers.fulfilled, (state, action) => {
+				state.status = 'succeeded';
+				state.error = '';
+				state.users = action.payload;
+			})
+			.addCase(getUsers.rejected, (state, action) => {
+				state.status = 'failed';
+				state.error = action.payload;
+			})
+
+			.addCase(getAuthorById.pending, (state) => {
+				state.status = 'loading';
+				state.error = '';
+			})
+			.addCase(getAuthorById.fulfilled, (state, action) => {
+				state.status = 'succeeded';
+				state.error = '';
+				state.author = action.payload;
+			})
+			.addCase(getAuthorById.rejected, (state, action) => {
+				state.status = 'failed';
+				state.error = action.payload;
+			})
+
+
+			.addCase(updateUnreadMessage.pending, (state) => {
+				state.status = 'loading';
+				state.error = '';
+			})
+			.addCase(updateUnreadMessage.fulfilled, (state, action) => {
+				state.status = 'succeeded';
+				state.error = '';
+			})
+			.addCase(updateUnreadMessage.rejected, (state, action) => {
 				state.status = 'failed';
 				state.error = action.payload;
 			});
 	},
 });
 
-export const { login, logout, setError, setStatus } = userSlice.actions;
+export const { login, logout, setError, setStatus, setOnlineUsers } = userSlice.actions;
 export default userSlice.reducer;
